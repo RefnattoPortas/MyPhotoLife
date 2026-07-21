7c38887971e0ae90a7053ba96b085ce2d2083508
Developer <dev@myphotolife.com>
2026-07-21 20:14:31 -0300

    chore: remove leftover verify.js

--- verify.js
+++
@@ -1,5 +0,0 @@
-const bcrypt = require('bcryptjs');
-const hash = '$2b$12$sJoIAHPzKgQA3LlEi9QwBulqK.U0Q6A7HFmsm5znwcpmue3mMDGfu';
-console.log('Hash do banco:', hash);
-console.log('Testando 13245678:', bcrypt.compareSync('13245678', hash));
-console.log('Testando 12345678:', bcrypt.compareSync('12345678', hash));
